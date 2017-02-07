aee4506c1fabc2ec20a993c059b0fe4a1c531451
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2017-02-06 23:24:50 -0500

    cobra skeleton

---
+++ cmd/put.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// putCmd represents the put command
+var putCmd = &cobra.Command{
+	Use:   "put",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		fmt.Println("put called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(putCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// putCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	putCmd.Flags().StringP("kmskeyid", "k", "", "KMS CMK ID to use for encryption")
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+// Copyright © 2017 Stelligent Systems
+
+package main
+
+import "github.com/stelligent/crossing-go/cmd"
+
+func main() {
+	cmd.Execute()
+}
