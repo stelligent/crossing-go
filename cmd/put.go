ee72da10059896c4195b7ae9d973da8e31c12d6f
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2017-02-09 00:43:05 -0500

    implement upload

--- cmd/put.go
+++ cmd/put.go
@@ -1,42 +1,88 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3crypto"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/stelligent/crossing-go/crypto"
 )
 
 // putCmd represents the put command
 var putCmd = &cobra.Command{
-	Use:   "put",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "put [source] [S3 URL]",
+	Short: "Upload a file to S3",
+	Long: `Using Client Side Encryption (CSE), encrypt and upload
+a file to S3.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 2 {
 			cmd.UsageFunc()(cmd)
-			return
+			os.Exit(1)
 		}
-		// TODO: Work your own magic here
-		fmt.Println("put called")
+
+		s3bucket, s3object, err := parseS3Url(args[1])
+
+		if err != nil {
+			cmd.UsageFunc()(cmd)
+			os.Exit(1)
+		}
+		err = putS3Cse(s3bucket, s3object, viper.GetString("kmskeyid"), args[0])
+		if err != nil {
+			fmt.Printf("err uploading file: %s\n", err)
+			os.Exit(1)
+		}
 	},
+}
+
+func putS3Cse(bucket string, key string, kmskeyid string, source string) error {
+	file, err := os.Open(source)
+	if err != nil {
+		return fmt.Errorf("err opening file: %s", err)
+	}
+	defer file.Close()
+	fileInfo, _ := file.Stat()
+	size := fileInfo.Size()
+	buffer := make([]byte, size)
+	file.Read(buffer)
+	fileBytes := bytes.NewReader(buffer)
+	fileType := http.DetectContentType(buffer)
+	params := &s3.PutObjectInput{
+		Bucket: &bucket,
+		Key:    &key,
+		Body:   fileBytes,
+		// ContentLength: aws.Int64(size),
+		ContentType: aws.String(fileType),
+	}
+	sess := session.New()
+	cmkID := kmskeyid
+	// Create the KeyProvider
+	handler := s3crypto.NewKMSKeyGenerator(kms.New(sess), cmkID)
+
+	// Create an encryption and decryption client
+	svc := s3crypto.NewEncryptionClient(sess, crosscrypto.AESCBCContentCipherBuilder(handler))
+
+	_, err = svc.PutObject(params)
+	if err != nil {
+		return fmt.Errorf("bad response: %s", err)
+	}
+	return nil
 }
 
 func init() {
 	RootCmd.AddCommand(putCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// putCmd.PersistentFlags().String("foo", "", "A help for foo")
-
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	putCmd.Flags().StringP("kmskeyid", "k", "", "KMS CMK ID to use for encryption")
+	viper.BindPFlag("kmskeyid", putCmd.Flags().Lookup("kmskeyid"))
 
 }
