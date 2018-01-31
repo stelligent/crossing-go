4f49445c3e9fa95599b17f57ffeac65d44267961
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2018-01-30 23:09:42 -0500

    Copy sdk implementation
    aws-sdk-go brought in a AES CBC implementation after the first version
    of crossing-go went out.
    
    Copying it here because more eyes on their implementation should mean
    better crypto code

--- crypto/aes_cbc.go
+++ crypto/aes_cbc.go
@@ -4,150 +4,189 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"fmt"
 	"io"
-	"io/ioutil"
 
 	"github.com/aws/aws-sdk-go/service/s3/s3crypto"
 )
 
-// Appends padding.
-func pkcs7Pad(data []byte, blocklen int) ([]byte, error) {
-	if blocklen <= 0 {
-		return nil, fmt.Errorf("invalid blocklen %d", blocklen)
-	}
-	padlen := 1
-	for ((len(data) + padlen) % blocklen) != 0 {
-		padlen = padlen + 1
-	}
-
-	pad := bytes.Repeat([]byte{byte(padlen)}, padlen)
-	return append(data, pad...), nil
-}
-
-// Returns slice of the original data without padding.
-func pkcs7Unpad(data []byte, blocklen int) ([]byte, error) {
-	if blocklen <= 0 {
-		return nil, fmt.Errorf("invalid blocklen %d", blocklen)
-	}
-	if len(data)%blocklen != 0 || len(data) == 0 {
-		return nil, fmt.Errorf("invalid data len %d", len(data))
-	}
-	padlen := int(data[len(data)-1])
-	if padlen > blocklen || padlen == 0 {
-		return nil, fmt.Errorf("invalid padding")
-	}
-	// check padding
-	pad := data[len(data)-padlen:]
-	for i := 0; i < padlen; i++ {
-		if pad[i] != byte(padlen) {
-			return nil, fmt.Errorf("invalid padding")
-		}
-	}
-
-	return data[:len(data)-padlen], nil
-}
-
-// AESCBC Symmetric encryption algorithm. Since Golang designed this
-// with only TLS in mind. We have to load it all into memory meaning
-// this isn't streamed.
+// AESCBC is a symmetric crypto algorithm. This algorithm
+// requires a padder due to CBC needing to be of the same block
+// size. AES CBC is vulnerable to Padding Oracle attacks and
+// so should be avoided when possible.
 type aesCBC struct {
-	decrypter cipher.BlockMode
 	encrypter cipher.BlockMode
-	iv        []byte
+	decrypter cipher.BlockMode
+	padder    s3crypto.Padder
 }
 
-// NewAESCBC creates a new AES CBC cipher. Expects keys to be of
+// newAESCBC creates a new AES CBC cipher. Expects keys to be of
 // the correct size.
-//
-// Example:
-//
-//	cd := &s3crypto.CipherData{
-//		Key: key,
-//		"IV": iv,
-//	}
-//	cipher, err := crosscrypto.NewAESCBC(cd)
-func newAESCBC(cd s3crypto.CipherData) (s3crypto.Cipher, error) {
+func newAESCBC(cd s3crypto.CipherData, padder s3crypto.Padder) (s3crypto.Cipher, error) {
 	block, err := aes.NewCipher(cd.Key)
 	if err != nil {
 		return nil, err
 	}
 
-	aescbcDecrypter := cipher.NewCBCDecrypter(block, cd.IV)
-	if err != nil {
-		return nil, err
-	}
+	encrypter := cipher.NewCBCEncrypter(block, cd.IV)
+	decrypter := cipher.NewCBCDecrypter(block, cd.IV)
 
-	aescbcEncrypter := cipher.NewCBCEncrypter(block, cd.IV)
-	if err != nil {
-		return nil, err
-	}
-
-	return &aesCBC{aescbcDecrypter, aescbcEncrypter, cd.IV}, nil
+	return &aesCBC{encrypter, decrypter, padder}, nil
 }
 
-// Encrypt will encrypt the data using AES CBC
-// Tag will be included as the last 16 bytes of the slice
+// Encrypt will encrypt the data using AES CBC by returning
+// an io.Reader. The io.Reader will encrypt the data as Read
+// is called.
 func (c *aesCBC) Encrypt(src io.Reader) io.Reader {
 	reader := &cbcEncryptReader{
 		encrypter: c.encrypter,
-		iv:        c.iv,
 		src:       src,
+		padder:    c.padder,
 	}
 	return reader
 }
 
 type cbcEncryptReader struct {
 	encrypter cipher.BlockMode
-	iv        []byte
 	src       io.Reader
-	buf       *bytes.Buffer
+	padder    s3crypto.Padder
+	size      int
+	buf       bytes.Buffer
 }
 
+// Read will read from our io.Reader and encrypt the data as necessary.
+// Due to padding, we have to do some logic that when we encounter an
+// end of file to pad properly.
 func (reader *cbcEncryptReader) Read(data []byte) (int, error) {
-	if reader.buf == nil {
-		b, err := ioutil.ReadAll(reader.src)
-		if err != nil {
-			return len(b), err
+	n, err := reader.src.Read(data)
+	reader.size += n
+	blockSize := reader.encrypter.BlockSize()
+	reader.buf.Write(data[:n])
+
+	if err == io.EOF {
+		b := make([]byte, getSliceSize(blockSize, reader.buf.Len(), len(data)))
+		n, err = reader.buf.Read(b)
+		if err != nil && err != io.EOF {
+			return n, err
 		}
-		b, _ = pkcs7Pad(b, reader.encrypter.BlockSize())
-		reader.encrypter.CryptBlocks(b, b)
-		reader.buf = bytes.NewBuffer(b)
+		// The buffer is now empty, we can now pad the data
+		if reader.buf.Len() == 0 {
+			b, err = reader.padder.Pad(b[:n], reader.size)
+			if err != nil {
+				return n, err
+			}
+			n = len(b)
+			err = io.EOF
+		}
+		// We only want to encrypt if we have read anything
+		if n > 0 {
+			reader.encrypter.CryptBlocks(data, b)
+		}
+		return n, err
 	}
 
-	return reader.buf.Read(data)
+	if err != nil {
+		return n, err
+	}
+
+	if size := reader.buf.Len(); size >= blockSize {
+		nBlocks := size / blockSize
+		if size > len(data) {
+			nBlocks = len(data) / blockSize
+		}
+
+		if nBlocks > 0 {
+			b := make([]byte, nBlocks*blockSize)
+			n, _ = reader.buf.Read(b)
+			reader.encrypter.CryptBlocks(data, b[:n])
+		}
+	} else {
+		n = 0
+	}
+	return n, nil
 }
 
-// Decrypt will decrypt the data using AES GCM
+// Decrypt will decrypt the data using AES CBC
 func (c *aesCBC) Decrypt(src io.Reader) io.Reader {
 	return &cbcDecryptReader{
 		decrypter: c.decrypter,
-		iv:        c.iv,
 		src:       src,
+		padder:    c.padder,
 	}
 }
 
 type cbcDecryptReader struct {
 	decrypter cipher.BlockMode
-	iv        []byte
 	src       io.Reader
-	buf       *bytes.Buffer
+	padder    s3crypto.Padder
+	buf       bytes.Buffer
 }
 
+// Read will read from our io.Reader and decrypt the data as necessary.
+// Due to padding, we have to do some logic that when we encounter an
+// end of file to pad properly.
 func (reader *cbcDecryptReader) Read(data []byte) (int, error) {
-	if reader.buf == nil {
-		b, err := ioutil.ReadAll(reader.src)
-		if err != nil {
-			return len(b), err
-		}
-		reader.decrypter.CryptBlocks(b, b)
-		if err != nil {
-			return len(b), err
-		}
-		b, _ = pkcs7Unpad(b, reader.decrypter.BlockSize())
+	n, err := reader.src.Read(data)
+	blockSize := reader.decrypter.BlockSize()
+	reader.buf.Write(data[:n])
 
-		reader.buf = bytes.NewBuffer(b)
+	if err == io.EOF {
+		b := make([]byte, getSliceSize(blockSize, reader.buf.Len(), len(data)))
+		n, err = reader.buf.Read(b)
+		if err != nil && err != io.EOF {
+			return n, err
+		}
+		// We only want to decrypt if we have read anything
+		if n > 0 {
+			reader.decrypter.CryptBlocks(data, b)
+		}
+
+		if reader.buf.Len() == 0 {
+			b, err = reader.padder.Unpad(data[:n])
+			n = len(b)
+			if err != nil {
+				return n, err
+			}
+			err = io.EOF
+		}
+		return n, err
 	}
 
-	return reader.buf.Read(data)
+	if err != nil {
+		return n, err
+	}
+
+	if size := reader.buf.Len(); size >= blockSize {
+		nBlocks := size / blockSize
+		if size > len(data) {
+			nBlocks = len(data) / blockSize
+		}
+		// The last block is always padded. This will allow us to unpad
+		// when we receive an io.EOF error
+		nBlocks -= blockSize
+
+		if nBlocks > 0 {
+			b := make([]byte, nBlocks*blockSize)
+			n, _ = reader.buf.Read(b)
+			reader.decrypter.CryptBlocks(data, b[:n])
+		} else {
+			n = 0
+		}
+	}
+
+	return n, nil
+}
+
+// getSliceSize will return the correct amount of bytes we need to
+// read with regards to padding.
+func getSliceSize(blockSize, bufSize, dataSize int) int {
+	size := bufSize
+	if bufSize > dataSize {
+		size = dataSize
+	}
+	size = size - (size % blockSize) - blockSize
+	if size <= 0 {
+		size = blockSize
+	}
+
+	return size
 }
