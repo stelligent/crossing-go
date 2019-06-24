c6a7cfa972cf6825e4c511a34f5dc02ec2a2f7ff
Carlos Luna <clunas888@outlook.com>
2019-06-24 15:21:47 -0600

    Start tests from scratch

--- cmd/get_test.go
+++ cmd/get_test.go
@@ -1,24 +1,35 @@
 package cmd
 
-import "testing"
+import (
+	"testing"
 
-func Test_getS3Cse(t *testing.T) {
-	type args struct {
-		s3bucket string
-		s3object string
-		filedest string
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+)
+
+func TestGet_getS3Cse(t *testing.T) {
+	type fields struct {
+		Client          s3iface.S3API
+		Bucket          string
+		Key             string
+		FileDestination string
 	}
 	tests := []struct {
 		name    string
-		args    args
+		fields  fields
 		wantErr bool
 	}{
-		{"Fail on garbage input", args{"nosuchbucket", "nosuchobject", "notafile"}, true},
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := getS3Cse(tt.args.s3bucket, tt.args.s3object, tt.args.filedest); (err != nil) != tt.wantErr {
-				t.Errorf("getS3Cse() error = %v, wantErr %v", err, tt.wantErr)
+			g := &Get{
+				Client:          tt.fields.Client,
+				Bucket:          tt.fields.Bucket,
+				Key:             tt.fields.Key,
+				FileDestination: tt.fields.FileDestination,
+			}
+			if err := g.getS3Cse(); (err != nil) != tt.wantErr {
+				t.Errorf("Get.getS3Cse() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
